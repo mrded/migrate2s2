c8e6d490e9684763f7d52420e34f4d40d1a555e8
Dmitry Demenchuk <dmitry@demenchuk.me>
2017-06-22 20:04:54 +0100

    Init arguments.

--- Main.hs
+++ Main.hs
@@ -1,4 +1,54 @@
-module Main where
+import Options.Applicative
+import Data.Semigroup ((<>))
+
+data Args = Args
+  { database   :: String 
+  , host       :: String
+  , user       :: String
+  , password   :: String
+  , port       :: Int }
+
+args :: Parser Args
+args = Args
+      <$> strOption
+          ( long "database"
+         <> short 'D'
+         <> help "Database to use."
+         <> metavar "name" )
+      <*> strOption
+          ( long "host"
+         <> short 'h'
+         <> help "Connect to host."
+         <> showDefault
+         <> value "localhost"
+         <> metavar "name" )
+      <*> strOption
+          ( long "user"
+         <> short 'u'
+         <> help "user for login."
+         <> showDefault
+         <> value "root"
+         <> metavar "name" )
+      <*> strOption
+          ( long "password"
+         <> short 'p'
+         <> help "Password to use when connecting to server."
+         <> showDefault
+         <> value "root"
+         <> metavar "name" )
+      <*> option auto
+          ( long "port"
+         <> short 'P'
+         <> help "Port number to use for connection."
+         <> showDefault
+         <> value 3306
+         <> metavar "#" )
 
 main :: IO ()
-main = putStrLn "Hello, Haskell!"
+main = greet =<< execParser opts
+  where
+    opts = info (args <**> helper)
+      ( fullDesc <> progDesc "Migrates images to S3" )
+
+greet :: Args -> IO ()
+greet _ = return ()
